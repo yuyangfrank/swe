9ec3ccb1033fae8b810e816d8917ea0deaaf7643
Yang <yuyangfrank@gmail.com>
2022-01-27 00:23:24 +0800

    Create DB and inital dataset

---
+++ target/classes/db/migration/V1__create_tables.sql
@@ -0,0 +1,5 @@
+CREATE table if not exists user_salary
+(
+    name char(36) primary key,
+    salary Decimal (10, 2)
+);
---
+++ target/classes/db/migration/V2__insert_records.sql
@@ -0,0 +1,12 @@
+INSERT into user_salary (name, salary) VALUES ("John", 2500.05);
+INSERT into user_salary (name, salary) VALUES ("Mary Posa", 4000.00);
+INSERT into user_salary (name, salary) VALUES ("John 0", 500.00);
+INSERT into user_salary (name, salary) VALUES ("John 1", 1500.00);
+INSERT into user_salary (name, salary) VALUES ("John 2", 2500.00);
+INSERT into user_salary (name, salary) VALUES ("John 3", 3500.00);
+INSERT into user_salary (name, salary) VALUES ("John 4", 4500.00);
+INSERT into user_salary (name, salary) VALUES ("John 0-5", 500.05);
+INSERT into user_salary (name, salary) VALUES ("John 1-5", 1500.05);
+INSERT into user_salary (name, salary) VALUES ("John 2-5", 2500.05);
+INSERT into user_salary (name, salary) VALUES ("John 3-5", 3500.05);
+INSERT into user_salary (name, salary) VALUES ("John 4-5", 4500.05);
